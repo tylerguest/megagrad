78e2386595ff11fc2cc76d42565f47ba1219b411
Tyler Guest <tylerguest@users.noreply.github.com>
2025-07-18 12:48:12 -0400

    - updated tensors to allow for arguments
    - added some testing

--- megagrad/tensor.py
+++ megagrad/tensor.py
@@ -1,41 +1,61 @@
-class Value:
-  """ stores a single scalar value and its gradient"""
+import numpy as np
 
-  def __init__(self, data, _children=(), _op='', label=''):
-    self.data = data
-    self.grad = 0.0
+class Tensor:
+  def __init__(self, data, *args, _children=(), _op='', label=''):
+    if args: data = (data,) + args
+    self.data = np.array(data, dtype=float)
+    self.grad = np.zeros_like(self.data)
     self._backward = lambda: None
     self._prev = set(_children)
     self._op = _op 
     self.label = label
+    self.shape = self.data.shape
   
   def __add__(self, other):
-    other = other if isinstance(other, Value) else Value(other)
-    out = Value(self.data + other.data, (self, other), '+')
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    out = Tensor(self.data + other.data, _children=(self, other), _op='+')
     def _backward():
       self.grad += out.grad
       other.grad += out.grad
     out._backward = _backward
     return out
   
+  def __sub__(self, other):
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    out = Tensor(self.data - other.data, _children=(self, other), _op='-')
+    def _backward():
+      self.grad += out.grad
+      other.grad -= out.grad
+    out._backward = _backward
+    return out
+  
   def __mul__(self, other):
-    other = other if isinstance(other, Value) else Value(other)
-    out = Value(self.data * other.data, (self, other), '*')
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    out = Tensor(self.data * other.data, _children=(self, other), _op='*')
     def _backward():
       self.grad += other.data * out.grad
       other.grad += self.data * out.grad
     out._backward = _backward
     return out
   
+  def __truediv__(self, other):
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    out = Tensor(self.data / other.data, _children=(self, other), _op='/')
+    def _backward():
+      self.grad += (1 / other.data, (self, other), '/')
+      other.grad -= (self.data / (other.data ** 2)) * out.grad
+    out._backward = _backward
+    return out
+
   def __pow__(self, other):
     assert isinstance(other, (int, float)), "only int/float powers for now"
-    out = Value(self.data**other, (self,), f'**{other}')
+    out = Tensor(self.data**other, _children=(self,), _op=f'**{other}')
     def _backward(): self.grad += (other * self.data**(other-1)) * out.grad
     out._backward = _backward
     return out
   
   def relu(self):
-    out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+    out = Tensor(np.maximum(0, self.data), _children=(self,), _op='ReLU')
     def _backward(): self.grad += (out.data > 0) * out.grad
     out._backward = _backward
     return out
@@ -49,9 +69,26 @@ class Value:
         for child in v._prev: build_topo(child)
         topo.append(v)
     build_topo(self)
-    self.grad = 1.0
+    self.grad = np.ones_like(self.data)
     for v in reversed(topo): v._backward()
   
+  def numpy(self): return self.data
+  def __iadd__(self, other): 
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    self.data += other.data
+    return self
+  def __isub__(self, other):
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    self.data -= other.data
+    return self
+  def __imul__(self, other):
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    self.data *= other.data
+    return self
+  def __idiv__(self, other):
+    other = other if isinstance(other, Tensor) else Tensor(other)
+    self.data /= other.data
+    return self
   def __neg__(self): return self * -1
   def __radd__(self, other): return self + other
   def __sub__(self, other): return self + (-other)
@@ -59,4 +96,4 @@ class Value:
   def __rmul__(self, other): return self * other
   def __truediv__(self, other): return self * other**-1
   def __rtruediv__(self, other): return other * self**-1
-  def __repr__(self): return f"Value(data={self.data}, grad={self.grad})"
+  def __repr__(self): return f"Tensor(shape={self.shape}, data={self.data}, grad={self.grad})"
